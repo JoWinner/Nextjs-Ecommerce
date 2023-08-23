d5553f316a846ea517317c66a21357ba5f5c9ee8
JoWinner <jowinner222@gmail.com>
2023-08-23 16:03:38 +0800

    Billboard fix

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,4 +1,4 @@
-import getBillboard from "@/actions/get-billboard";
+import getBillboards from "@/actions/get-billboards";
 import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product-list";
 import Billboard from "@/components/ui/billboard";
@@ -6,17 +6,19 @@ import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
-const billboardID = process.env.Home_Billboard;
-
-
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard(`${billboardID}`);
+  const billboards = await getBillboards();
+
+  const firstBillboard = billboards[0];
+
 
   return (
     <Container>
-      <div className="space-y-10 pb-10">
-        <Billboard data={billboard} />
+      <div className="space-y-10 pb-10">       
+          <div key={firstBillboard.id}>
+            <Billboard data={firstBillboard} />
+          </div>      
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
         </div>
